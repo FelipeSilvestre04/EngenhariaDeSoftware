24013acbc7083f401946780067d67e80ef6fea3e
tasso-canellas <168464454+tasso-canellas@users.noreply.github.com>
2025-09-10 15:48:14 -0300

    Testando Husky no Windows

---
+++ test.js
@@ -0,0 +1,11 @@
+import {test, describe} from "node:test";
+import assert from "node:assert";
+
+describe("Test", () => {
+    test("hello bro", ()=>{
+        const expected = "Hello World!";
+        const actual = 'Hello World!';
+
+        assert.strictEqual(actual, expected);
+    });
+});
