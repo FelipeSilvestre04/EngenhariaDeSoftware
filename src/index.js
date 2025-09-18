100d91794a1ae9ab09079721534e8f731069acb3
tasso-canellas <168464454+tasso-canellas@users.noreply.github.com>
2025-09-17 23:58:25 -0300

    chore: atualizando index.js pra testar no render

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import http from 'http';
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from Render!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
