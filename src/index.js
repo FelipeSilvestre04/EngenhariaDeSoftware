cb4d4d24f93b1338a233e2af5e13641379b44f94
tasso-canellas <168464454+tasso-canellas@users.noreply.github.com>
2025-09-25 00:30:20 -0300

    chore: correções na requisição. render tava em loop consultado o modelo

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(async (req, res) => {
-  const llmModel = await createModel("openai/gpt-oss-120b", 0.2);
-  const response = await consulta("Me responda apenas \"Sim\": ", "O Tasso é bom ou não é?", llmModel)
-  console.log("O Tasso é bom ou não é?")
-  console.log(response.content)
+    const llmModel = await createModel("openai/gpt-oss-120b", 0.2);
+    const response = await consulta("Me responda apenas \"Sim\": ", "O Tasso é bom ou não é?", llmModel);
+
+    
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(`O Tasso é bom ou não é? ${response.content}`);
 });
 
 server.listen(PORT, () => {
