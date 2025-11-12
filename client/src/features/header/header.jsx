35260166a2bab622843b5be20739eb6eb578f8d6
Abner <abnerdiniz90@gmail.com>
2025-11-12 12:49:50 -0300

    feat: adicionado header e logo

--- client/src/features/header/header.jsx
+++ client/src/features/header/header.jsx
@@ -1,10 +1,18 @@
 
 import React from 'react';
 import styles from './header.module.css'; 
+import logoSia from '../../assets/SecretarIA_cut.png';
 
 function Header({ onToggleClick }) { 
   return (
-    <header className={styles.headerContainer}>SecretarIA</header>
+
+    <header className={styles.headerContainer}>
+
+      <img src={logoSia} alt="Logo SecretarIA" className={styles.logo} />
+      <p className={styles.headerTitle}>SecretarIA</p>
+
+    </header>
+    
   );
 }
 
